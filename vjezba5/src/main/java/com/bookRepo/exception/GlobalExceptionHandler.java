ab3376509929ace99d71f9da9faef3cd5435b467
Filip Jurcevic <147091790+Filip25601@users.noreply.github.com>
2024-12-19 23:24:39 +0100

    vj5

---
+++ vjezba5/src/main/java/com/bookRepo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.bookRepo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<Object> handleBookNotFoundException(BookNotFoundException e) {
+        ApiException apiException = new ApiException(
+                e.getMessage(),
+                HttpStatus.NOT_FOUND,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+        return new ResponseEntity<>(apiException,HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(InvalidRequestException.class)
+    public ResponseEntity<Object> handleInvalidRequestException(InvalidRequestException e) {
+        ApiException apiException = new ApiException(
+                e.getMessage(),
+                HttpStatus.BAD_REQUEST,
+                ZonedDateTime.now(ZoneId.of("Z"))
+                );
+
+        return new ResponseEntity<>(apiException,HttpStatus.BAD_REQUEST);
+
+    }
+}
---
+++ vjezba5/src/main/java/com/bookRepo/exception/InvalidRequestException.java
@@ -0,0 +1,7 @@
+package com.bookRepo.exception;
+
+public class InvalidRequestException extends RuntimeException{
+        public InvalidRequestException(String message) {
+            super(message);
+    }
+}
